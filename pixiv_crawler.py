2dc9bf267d5deb01d24dff8a3e939040b4bcd3f4
AAA <35992542+TNTcraftHIM@users.noreply.github.com>
2022-12-26 05:59:52 +1100

    Added traceback for errors and added progress bar

--- pixiv_crawler.py
+++ pixiv_crawler.py
@@ -233,8 +233,8 @@ def auth_api(log_info=False):
         try:
             logger.info("Pixiv logged in as " + user_detail.user.name)
         except Exception as e:
-            traceback.print_exc()
-            logger.critical("Pixiv login failed due to error: " + str(e))
+            logger.critical("Pixiv login failed due to error: " +
+                            str(e) + "\n" + traceback.format_exc())
             exit()
 
 
@@ -298,72 +298,75 @@ def crawl_images(manual=False, force_update=False, dates=[None]):
     # crawl images:
     try:
         auth_api(False)
-        for date in dates:
+        for i in range(len(dates)):
+            crawler_status = 'crawling automatically since update_interval of ' + str(update_interval) + " has been reached" if not manual else 'crawling manually {}{}'.format(
+                'and forcing updates ' if force_update else '', 'to crawl from date {} to {} [%{} completed]'.format(dates[0], dates[-1], round(i/len(dates)*100, 2)) if dates[0] != None else '')
             for mode in get_list(ranking_modes):
-                if date == None:
+                if dates[i] == None:
                     next_qs = {"mode": mode}
                 else:
-                    next_qs = {"mode": mode, "date": date}
+                    next_qs = {"mode": mode, "date": dates[i]}
                 while next_qs:
                     json_result = api.illust_ranking(**next_qs)
-                    for illust in json_result.illusts:
-                        if (illust.type == "manga"):
-                            continue
-                        if (not allow_multiple_pages and illust.page_count > 1):
-                            continue
-                        # if any tag in excluding_tags_list is in illust.tags, skip
-                        for tag in illust.tags:
-                            if ((tag.name is not None and tag.name.lower() in excluding_tags_list) or (tag.translated_name is not None and tag.translated_name.lower() in excluding_tags_list)):
+                    if json_result.illusts != None:
+                        for illust in json_result.illusts:
+                            if (illust.type == "manga"):
                                 continue
-                        urls = []
-                        url = None
-                        if illust.page_count == 1:
-                            if (download_quality == "original"):
-                                url = illust.meta_single_page.original_image_url
-                            elif (download_quality == "large"):
-                                url = illust.image_urls.large
-                            else:
-                                url = illust.image_urls.medium
-                        if (url == None):
-                            for images in illust.meta_pages:
+                            if (not allow_multiple_pages and illust.page_count > 1):
+                                continue
+                            # if any tag in excluding_tags_list is in illust.tags, skip
+                            for tag in illust.tags:
+                                if ((tag.name is not None and tag.name.lower() in excluding_tags_list) or (tag.translated_name is not None and tag.translated_name.lower() in excluding_tags_list)):
+                                    continue
+                            urls = []
+                            url = None
+                            if illust.page_count == 1:
                                 if (download_quality == "original"):
-                                    url = images.image_urls.original
+                                    url = illust.meta_single_page.original_image_url
                                 elif (download_quality == "large"):
-                                    url = images.image_urls.large
+                                    url = illust.image_urls.large
                                 else:
-                                    url = images.image_urls.medium
-                                urls.append(url)
-                                if (not get_all_multiple_pages):
-                                    break
-                        else:
-                            urls = [url]
-                        for i in range(len(urls)):
-                            url = urls[i]
-                            pk = str(illust.id) + "_" + str(i)
-                            local_filename = ""
-                            if (store_mode == "full"):
-                                extension = os.path.splitext(url)[1]
-                                local_filename = slugify(
-                                    f"{str(illust.id)}_{illust.user.name}_{illust.title}_p{str(i)}", True) + extension
-                                if download_reverse_proxy != "":
-                                    url = url.replace(
-                                        "i.pximg.net", download_reverse_proxy)
-                                if(api.download(url, path=download_folder, name=local_filename)):
-                                    download_count += 1
-                                local_filename = download_folder + os.sep + local_filename
-                            data = {"id": illust.id, "author_id": illust.user.id, "author_name": illust.user.name, "title": illust.title, "page_no": i,
-                                    "page_count": illust.page_count, "r18": illust.x_restrict, "ai_type": illust.illust_ai_type, "tags": illust.tags, "url": url, "local_filename": local_filename}
-                            # insert into database
-                            image_count += 1
-                            if (insertDB(pk, data, force_update)):
-                                db_count += 1
-                    next_qs = api.parse_qs(json_result.next_url)
+                                    url = illust.image_urls.medium
+                            if (url == None):
+                                for images in illust.meta_pages:
+                                    if (download_quality == "original"):
+                                        url = images.image_urls.original
+                                    elif (download_quality == "large"):
+                                        url = images.image_urls.large
+                                    else:
+                                        url = images.image_urls.medium
+                                    urls.append(url)
+                                    if (not get_all_multiple_pages):
+                                        break
+                            else:
+                                urls = [url]
+                            for i in range(len(urls)):
+                                url = urls[i]
+                                pk = str(illust.id) + "_" + str(i)
+                                local_filename = ""
+                                if (store_mode == "full"):
+                                    extension = os.path.splitext(url)[1]
+                                    local_filename = slugify(
+                                        f"{str(illust.id)}_{illust.user.name}_{illust.title}_p{str(i)}", True) + extension
+                                    if download_reverse_proxy != "":
+                                        url = url.replace(
+                                            "i.pximg.net", download_reverse_proxy)
+                                    if(api.download(url, path=download_folder, name=local_filename)):
+                                        download_count += 1
+                                    local_filename = download_folder + os.sep + local_filename
+                                data = {"id": illust.id, "author_id": illust.user.id, "author_name": illust.user.name, "title": illust.title, "page_no": i,
+                                        "page_count": illust.page_count, "r18": illust.x_restrict, "ai_type": illust.illust_ai_type, "tags": illust.tags, "url": url, "local_filename": local_filename}
+                                # insert into database
+                                image_count += 1
+                                if (insertDB(pk, data, force_update)):
+                                    db_count += 1
                     if (not get_all_ranking_pages):
                         break
+                    next_qs = api.parse_qs(json_result.next_url)
         last_update_timestamp = time.time()
     except Exception as e:
-        traceback.print_exc()
-        logger.error("Aborting crawler task due to error: " + str(e))
+        logger.error("Aborting crawler task due to error: " +
+                     str(e) + "\n" + traceback.format_exc())
     logger.info(
         f"Crawled {image_count} images, {db_count} images added to database, {download_count} images downloaded")
     crawler_status = "idle"
