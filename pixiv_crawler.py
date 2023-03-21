3a18144d7ae8df8aab98858bd5ba7a7ef0b9a2bf
AAA <35992542+TNTcraftHIM@users.noreply.github.com>
2023-03-22 00:50:42 +1100

    Added more logging when compressing images

--- pixiv_crawler.py
+++ pixiv_crawler.py
@@ -465,6 +465,7 @@ def compress_images(image_quality: int = 75, force_compress: bool = False, delet
     results = cursor_to_dict(cursor.execute(
         "SELECT * FROM pictures WHERE local_filename != ''{}".format(" AND local_filename_compressed = ''" if not force_compress else "")))
     crawler_status = "compressing images"
+    logger.info("Image compression task started with quality {}%".format(image_quality))
     count = 0
     try:
         for image in results:
@@ -499,7 +500,8 @@ def compress_images(image_quality: int = 75, force_compress: bool = False, delet
     except Exception as e:
         logger.log(logging.ERROR,
                    "Aborting image compression task due to error: " + str(e) + "\n" + traceback.format_exc())
-    logger.log(logging.INFO, "Compressed {} images".format(count))
+    logger.log(logging.INFO, "Compressed {} images, {} images skipped and removed".format(
+        count, len(results) - count))
     crawler_status = "idle"
 
 
